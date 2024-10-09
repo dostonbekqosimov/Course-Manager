a46c88d419840e7cc9e2d3c97debb5d0dd908dc4
Doston Sulaymon <dostonqosimiy19@gmail.com>
2024-10-09 21:38:11 +0500

    Initial Commit

---
+++ src/main/java/code/doston/CoruseManagerApplication.java
@@ -0,0 +1,13 @@
+package code.doston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoruseManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoruseManagerApplication.class, args);
+    }
+
+}
